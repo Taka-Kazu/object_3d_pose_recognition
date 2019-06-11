8659ee51b61d0828ba42bb09ee390473c097ac0b
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-06-11 12:38:32 +0900

    change uv points from double to integer

--- utils/bounding_box_2d.py
+++ utils/bounding_box_2d.py
@@ -6,10 +6,10 @@ import numpy as np
 class BoundingBox2d:
     def __init__(self, data):
         self.bb = np.array(data)
-        self.xmin = self.bb[0]
-        self.ymin = self.bb[1]
-        self.xmax = self.bb[2]
-        self.ymax = self.bb[3]
+        self.xmin = int(self.bb[0])
+        self.ymin = int(self.bb[1])
+        self.xmax = int(self.bb[2])
+        self.ymax = int(self.bb[3])
         #print('2D BBox (xmin, xmax, ymin, ymax): %f, %f, %f, %f' % (self.bb2d.xmin, self.bb2d.xmax, self.bb2d.ymin, self.bb2d.ymax))
 
     def get_hull(self):
