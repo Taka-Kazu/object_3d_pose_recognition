f43fef6c11cdc939595a7bf34f884e0fd080251d
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-07-24 18:56:46 +0900

    add summary writer

--- model/train.py
+++ model/train.py
@@ -3,14 +3,21 @@
 
 from __future__ import print_function
 import argparse
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from torch.utils.tensorboard import SummaryWriter
 
 from network import Network
 from dataset import ObjectDataset
+
+log_directory = os.path.dirname(os.path.abspath(__file__)) + '/../logs'
+
+training_step = 0
+writer = SummaryWriter(log_dir=log_directory)
 
 class Net(nn.Module):
     def __init__(self):
@@ -33,7 +40,11 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1)
 
 def train(args, model, device, train_loader, optimizer, criterion, epoch):
+    global training_step
+    global writer
+
     model.train()
+
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -45,23 +56,31 @@ def train(args, model, device, train_loader, optimizer, criterion, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+        writer.add_scalar('loss', loss, training_step)
+        training_step += 1
 
 def test(args, model, device, test_loader, criterion):
+    global training_step
+    global writer
+
     model.eval()
+
     test_loss = 0
     correct = 0
+
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
             # test_loss += criterion(output, target, reduction='sum').item() # sum up batch loss
             test_loss += criterion(output, target) # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            prediction = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += prediction.eq(target.view_as(prediction)).sum().item()
     test_loss /= len(test_loader.dataset)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    writer.add_scalar('Accuracy', 100. * correct / len(test_loader.dataset), training_step)
 
 def main():
     # Training settings
@@ -114,6 +133,8 @@ def main():
         test(args, model, device, test_loader, criterion)
     if (args.save_model):
         torch.save(model.state_dict(),"mnist_cnn.pt")
+
+    writer.close()
 
 if __name__ == '__main__':
     main()
