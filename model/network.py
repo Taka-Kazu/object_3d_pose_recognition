ea9bdf7bae77e9353b2c1abe5d544cd7ca1d2203
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-07-25 16:57:07 +0900

    update network

--- model/network.py
+++ model/network.py
@@ -10,30 +10,37 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 
 class Network(nn.Module):
-    def __init__(self):
+    def __init__(self, class_num):
         super(Network, self).__init__()
-        self.input_2d = nn.Linear(12, 32)
-        self.fc1 = nn.Linear(32, 16)
-        self.input_3d = nn.Linear(12, 32)
-        self.fc2 = nn.Linear(32, 16)
-        self.fc3 = nn.Linear(32, 3)
+        self.input_2d = nn.Linear(class_num, 16)
+        self.fc1 = nn.Linear(16, 1)
+        self.input_3d = nn.Linear(14, 64)
+        self.fc2 = nn.Linear(64, 63)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(64, 64)
+        self.output = nn.Linear(64, 7)
 
-    def forward(self, x_2d, x_3d):
+    def forward(self, x_2d, x_3d, center):
         x = F.relu(self.input_2d(x_2d))
         x = F.relu(self.fc1(x))
         y = F.relu(self.input_3d(x_3d))
         y = F.relu(self.fc2(y))
         z = torch.cat([x, y], dim=0)
         z = F.relu(self.fc3(z))
-        return F.log_softmax(z, dim=0)
+        z = F.relu(self.fc4(z))
+        z = self.output(z)
+        z[0:len(center)] = z[0:len(center)] + center
+        return z
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
-    model = Network().to(device)
+    model = Network(2).to(device)
     print(model)
-    a = torch.rand(4, 3).reshape(-1).to(device)
+    a = torch.rand(2).to(device)
     print(a)
-    b = torch.rand(3, 4).reshape(-1).to(device)
+    b = torch.rand(14).to(device)
     print(b)
-    model.forward(a, b)
+    c = torch.rand(3).to(device)
+    print(c)
+    print(model.forward(a, b, c))
