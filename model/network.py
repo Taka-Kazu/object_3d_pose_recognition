7f7b0f92facf18d52e80c8e3fb6a3a82e9203a0c
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-07-25 19:33:46 +0900

    update network

--- model/network.py
+++ model/network.py
@@ -12,7 +12,8 @@ from torchvision import datasets, transforms
 class Network(nn.Module):
     def __init__(self, class_num):
         super(Network, self).__init__()
-        self.input_onehot = nn.Linear(class_num, 16)
+        self.class_num = class_num
+        self.input_onehot = nn.Linear(self.class_num, 16)
         self.fc1 = nn.Linear(16, 1)
         self.input_3d = nn.Linear(14, 64)
         self.fc2 = nn.Linear(64, 63)
@@ -20,18 +21,25 @@ class Network(nn.Module):
         self.fc4 = nn.Linear(64, 64)
         self.output = nn.Linear(64, 7)
 
-    def forward(self, onehot, x_3d):
-        x_3d_vec = x_3d[0:14]
-        x_3d_center = x_3d[14:17]
+    def forward(self, data):
+        x_3d_vec = data[:, 0:14]
+        x_3d_center = data[:, 14:17]
+
+        onehot = torch.eye(self.class_num)[data[:, 17].long()]
+        onehot = onehot.to(data.device)
+
         x = F.relu(self.input_onehot(onehot))
         x = F.relu(self.fc1(x))
+
         y = F.relu(self.input_3d(x_3d_vec))
         y = F.relu(self.fc2(y))
-        z = torch.cat([x, y], dim=0)
+
+        z = torch.cat([x, y], dim=1)
         z = F.relu(self.fc3(z))
         z = F.relu(self.fc4(z))
         z = self.output(z)
-        z[0:3] = z[0:3] + x_3d_center
+
+        z[:, 0:3] = z[:, 0:3] + x_3d_center
         return z
 
 if __name__ == '__main__':
@@ -39,8 +47,6 @@ if __name__ == '__main__':
     print(device)
     model = Network(3).to(device)
     print(model)
-    a = torch.rand(3).to(device)
-    print(a)
-    b = torch.rand(17).to(device)
+    b = torch.rand(18).to(device)
     print(b)
-    print(model.forward(a, b))
+    print(model.forward(b))
