45ec08b8b76d9bc5410e9a7bf0a9ba9bdb48a6c6
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-07-26 19:48:06 +0900

    add tensorboard

--- model/train.py
+++ model/train.py
@@ -4,6 +4,8 @@
 from __future__ import print_function
 import argparse
 import os
+import datetime
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,84 +14,66 @@ from torchvision import datasets, transforms
 from torch.utils.tensorboard import SummaryWriter
 
 from network import Network
-from dataset import ObjectDataset
+from dataset import ObjectDataset, ToTensor
 
-log_directory = os.path.dirname(os.path.abspath(__file__)) + '/../logs'
+log_directory = os.path.dirname(os.path.abspath(__file__)) + '/../logs/' + '{0:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
 
 training_step = 0
-writer = SummaryWriter(log_dir=log_directory)
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = nn.Linear(500, 10)
-        self.fc3 = nn.Linear(500, 5)
-        self.fc4 = nn.Linear(15, 10)
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
-        x = F.relu(self.fc1(x))
-        x = F.relu(torch.cat([self.fc2(x), self.fc3(x)], dim=1))
-        x = self.fc4(x)
-        return F.log_softmax(x, dim=1)
+train_writer = SummaryWriter(log_dir=log_directory+'/train')
+validation_writer = SummaryWriter(log_dir=log_directory+'/val')
 
 def train(args, model, device, train_loader, optimizer, criterion, epoch):
     global training_step
-    global writer
+    global train_writer
 
     model.train()
+
+    train_loss = 0
 
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
+        # print("shape: ", data.size())
         output = model(data)
         loss = criterion(output, target)
+        train_loss += loss
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-        writer.add_scalar('loss', loss, training_step)
+        # if batch_idx % args.log_interval == 0:
+        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        #         epoch, batch_idx * len(data), len(train_loader.dataset),
+        #         100. * batch_idx / len(train_loader), loss.item()))
+        # train_writer.add_scalar('loss', loss, training_step)
         training_step += 1
+    train_loss /= len(train_loader)
+    print('Train Epoch: {}\tAverage Loss: {:.6f}'.format(epoch, train_loss))
+    train_writer.add_scalar('loss', train_loss, training_step)
 
-def test(args, model, device, test_loader, criterion):
+def validation(args, model, device, test_loader, criterion):
     global training_step
-    global writer
+    global validation_writer
 
     model.eval()
 
     test_loss = 0
-    correct = 0
 
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            # test_loss += criterion(output, target, reduction='sum').item() # sum up batch loss
-            test_loss += criterion(output, target) # sum up batch loss
-            prediction = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += prediction.eq(target.view_as(prediction)).sum().item()
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    writer.add_scalar('Accuracy', 100. * correct / len(test_loader.dataset), training_step)
+            test_loss += criterion(output, target)
+    test_loss /= len(test_loader)
+    print('\nValidation set: Average loss: {:.4f}\n'.format(test_loss))
+    validation_writer.add_scalar('loss', test_loss, training_step)
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser = argparse.ArgumentParser(description='PyTorch')
+    parser.add_argument('--batch-size', type=int, default=16, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
                         help='learning rate (default: 0.001)')
@@ -97,7 +81,6 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
@@ -109,32 +92,33 @@ def main():
     kwargs = {'num_workers': 1, 'pin_memory': True}
 
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                     transform=transforms.Compose([
-                         transforms.ToTensor(),
-                         transforms.Normalize((0.1307,), (0.3081,))
-                     ])),
+        ObjectDataset(os.path.dirname(os.path.abspath(__file__)) + '/../kitti', 'train', transform=transforms.Compose([
+            ToTensor()
+        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                         transforms.ToTensor(),
-                         transforms.Normalize((0.1307,), (0.3081,))
-                     ])),
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+        ObjectDataset(os.path.dirname(os.path.abspath(__file__)) + '/../kitti', 'val', transform=transforms.Compose([
+            ToTensor()
+        ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
 
-    model = Net().to(device)
+    model = Network(3).to(device)
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    print('output log file to', log_directory)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, criterion, epoch)
-        test(args, model, device, test_loader, criterion)
-    if (args.save_model):
-        torch.save(model.state_dict(),"mnist_cnn.pt")
+        validation(args, model, device, test_loader, criterion)
 
-    writer.close()
+    if (args.save_model):
+        torch.save(model.state_dict(),"object_recognition.pt")
+
+    train_writer.close()
+    validation_writer.close()
 
 if __name__ == '__main__':
     main()
