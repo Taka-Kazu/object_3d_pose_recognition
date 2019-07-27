a723fefe18b4653295c5b57884011a13c053eb30
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-07-27 11:10:13 +0900

    update

--- utils/setup_euclidean_clustering.py
+++ utils/setup_euclidean_clustering.py
@@ -6,9 +6,11 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Compiler import Options
 
+import numpy
+
 Options.annotate = True
 
-ext_modules = [Extension("euclidean_clustering", ["euclidean_clustering.pyx"], language="c++"),
+ext_modules = [Extension("euclidean_clustering", ["euclidean_clustering.pyx"], include_dirs=[numpy.get_include()], language="c++"),
 ]
 
 setup(cmdclass={'build_ext': build_ext}, ext_modules=ext_modules)
